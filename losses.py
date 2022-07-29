1e4c8b34db196c258e0988e50e31e46f20c629c7
jinancsl <105267257+jinancsl@users.noreply.github.com>
2022-07-29 15:02:33 +0800

    Update losses.py

--- losses.py
+++ losses.py
@@ -25,25 +25,3 @@ class BCEDiceLoss(nn.Module):
         dice = (2. * intersection.sum(1) + smooth) / (input.sum(1) + target.sum(1) + smooth)
         dice = 1 - dice.sum() / num
         return 0.5 * bce + dice
-
-class BCE(nn.Module):
-    def __init__(self):
-        super(BCE, self).__init__()
-
-    def forward(self, input, target):
-        assert (input.size() == target.size())
-        pos = torch.eq(target, 1).float()
-        neg = torch.eq(target, 0).float()
-        # ing = ((torch.gt(target, 0) & torch.lt(target, 1))).float()
-
-        num_pos = torch.sum(pos)
-        num_neg = torch.sum(neg)
-        num_total = num_pos + num_neg
-
-        alpha = num_neg / num_total
-        beta = 1.1 * num_pos / num_total
-        # target pixel = 1 -> weight beta
-        # target pixel = 0 -> weight 1-beta
-        weights = alpha * pos + beta * neg
-        bce = F.binary_cross_entropy_with_logits(input, target, weights)
-        return bce
